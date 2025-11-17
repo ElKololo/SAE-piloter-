a20ff866c745d5826e5003e4ec8b6d86f82c3a30
ANDJIB Houdhayf <12302787@s30414.iutv.univ-paris13.fr>
2025-11-17 10:06:47 +0100

    Création d'un objet questions et propirétés images etc....

---
+++ script.js
@@ -0,0 +1,10 @@
+// Tâche : Créer un tableau questions contenant un premier objet questionTest
+const questions = [
+    {
+    // Tâche : Ajouter les propriétés image, question, options, correctAnswer
+    image: "assets/images/test.jpg",
+    question: "Quel joueur se trouve sur cette image ?",
+    options: ["Mbappé", "Messi", "Ronaldo"],
+    correctAnswer: "Mbappé"
+    }
+    ];
